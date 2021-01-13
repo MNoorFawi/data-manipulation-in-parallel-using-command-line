9f20add701b046d40a4e3b77fc0696214ce3470b
Muhammad N. Fawi <m.noor.fawi@gmail.com>
2021-01-13 12:13:11 +0400

    Update temp_fetch.sh

--- temp_fetch.sh
+++ temp_fetch.sh
@@ -3,9 +3,9 @@
 API_KEY="$1"
 tr ' ' '+' | 
 parallel -j400% --progress -C, 'curl -s "http://api.openweathermap.org/data/2.5/weather?q={}&appid='$API_KEY'"' | 
-jq '{name: .name, temperature: .main.temp}' | 
-json2csv | awk -F, '{print $1","$2-273.15}' | 
-header -r city,temperature | 
+jq -r '{name: .name, temperature: .main.temp} | [.name, .temperature] | @csv' |
+awk -F, '{print $1","$2-273.15}' |
+sed '1i city,temperature' |
 csvsql --query 'SELECT * FROM stdin ORDER BY temperature DESC;' | tee temp.csv | 
 head -n 6 | csvlook
 
